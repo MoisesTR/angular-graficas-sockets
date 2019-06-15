e03a4ff32bfeb1be40edf94b46ae78d30e000f28
mtrigueros <moisestrigueros@hotmail.com>
2019-06-15 11:42:37 -0600

    Incorporar proyecto de graficas son sockets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
+import {environment} from '../environments/environment';
+import {ChartsModule} from 'ng2-charts';
+import { GraficaComponent } from './components/grafica/grafica.component';
+import {HttpClientModule} from '@angular/common/http';
+const config: SocketIoConfig = { url: environment.wsUrl, options: {} };
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraficaComponent
   ],
   imports: [
     BrowserModule
+    , SocketIoModule.forRoot(config)
+    , ChartsModule
+    , HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/grafica/grafica.component.ts
@@ -0,0 +1,42 @@
+import {Component, OnInit} from '@angular/core';
+import {ChartDataSets} from 'chart.js';
+import {Label} from 'ng2-charts';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+import {WebsocketService} from '../../services/websocket.service';
+
+@Component({
+  selector: 'app-grafica',
+  templateUrl: './grafica.component.html',
+  styleUrls: ['./grafica.component.css']
+})
+export class GraficaComponent implements OnInit {
+
+  public lineChartData: ChartDataSets[] = [
+    { data: [0, 0, 0, 0], label: 'Ventas' }
+  ];
+  public lineChartLabels: Label[] = ['Enero', 'Febrero', 'Marzo', 'Abril'];
+
+  constructor(private httpClient: HttpClient, private weService: WebsocketService) {
+
+  }
+
+  ngOnInit(): void {
+    this.getDataGrafica();
+    this.escucharSockets();
+  }
+
+  getDataGrafica() {
+    this.httpClient.get(environment.wsUrl + '/grafica').subscribe(
+      (grafica: any) => this.lineChartData = grafica);
+  }
+
+  escucharSockets() {
+    this.weService.listen('cambio-grafica').subscribe(
+      (data: any) => {
+        console.log('Socket', data);
+        this.lineChartData = data;
+      }
+    );
+  }
+}
